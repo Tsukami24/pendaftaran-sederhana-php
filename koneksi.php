9bbc78947f85603518446aacfb25f80764049efe
Tsukami24 <atalahfakhri24@gmail.com>
2024-05-20 21:31:41 +0700

    first edit

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
 
 $koneksi = new mysqli('localhost', 'root', '', 'pendaftaran_sederhana');
 if ($koneksi) {
-    // echo "Koneksi Berhasil";
+    echo "Koneksi Berhasil";
 }else{
     echo $koneksi->error;
 }
